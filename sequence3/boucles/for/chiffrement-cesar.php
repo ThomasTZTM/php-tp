b959bc8d3be2141f9a0ff30fcd29e05817dd6c7d
Thomas Michelin <thomassteam122@gmail.com>
2023-10-18 11:52:21 +0200

    Modification

---
+++ sequence3/boucles/for/chiffrement-cesar.php
@@ -0,0 +1,14 @@
+<?php
+
+$chaine=readline("Entrer votre phrase à crypter : ");
+$taille_chaine=strlen($chaine);
+$cle=readline("Entrer une clé : ");
+
+// Lettre minuscule = 26
+// Lettre majuscule = 26
+
+
+
+
+
+
