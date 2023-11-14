bd4103f978864abb7a72934041d53075adb67163
Thomas Michelin <thomassteam122@gmail.com>
2023-11-14 08:55:04 +0100

    Modification

--- sequence4/tableau-index/pourcentage-avancement.php
+++ sequence4/tableau-index/pourcentage-avancement.php
@@ -1,1 +1,30 @@
 <?php
+$prog = 0;
+$t = 0;
+$a = array_fill(0,10000000,5);
+echo "debut du traitement\n";
+echo "En cours \n";
+foreach ($a as $item){
+    $prog++;
+    if(round((100*$prog)/10000000)<>$t){
+        $val=round((100*$prog)/10000000);
+        if ($val<100){
+            echo "\033[41m$val%\033[0m \n";
+        }
+        if ($val==100){
+            echo "\033[42m$val%\033[0m\n";
+        }
+    }
+    $t = round((100*$prog)/10000000);
+}
+echo "\n10000000 valeur on été traitées !";
+
+
+// \033[42m \033[0m = vert
+// \033[41m \033[0m= rouge
+
+
+
+
+
+
