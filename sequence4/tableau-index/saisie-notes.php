430f15a6f3f4700a34fd21979048b77c899dca96
Thomas Michelin <thomassteam122@gmail.com>
2023-11-14 08:27:04 +0100

    Modification

--- sequence4/tableau-index/saisie-notes.php
+++ sequence4/tableau-index/saisie-notes.php
@@ -1,11 +1,36 @@
 <?php
 
 $notes=[];
-while (true){
+$sup10=[];
+$moy=0;
+$cpt=0;
+$notesupa10=0;
+$rep=true;
+while ($rep==true){
     $note=readline("Entrer une note (q pour arrêter) : ");
-    while ($note>20 or $note<0){
-        echo "Erreur de format !";
-        $note=readline("Entrer une note (q pour arrêter) : ");
+    if ($note == "q"){
+        $rep = false;
+        echo "\nLes notes saisies sont : ";
+        foreach($notes as $n){
+            echo " $n ";
+            if($n>=10){
+                $sup10[]=$n;
+                $cpt++;
+                $moy=$moy+$n;
+                $notesupa10++;
+            }
+        }
+    }else{
+        while ($note>20 or $note<0){
+            echo "Erreur de format ! \n";
+            $note=readline("Entrer une note (q pour arrêter) : ");
+        }
     }
-    $notes[]=
+    $notes[]=$note;
+}
+if ($notesupa10>0){
+    $moy=$moy/$cpt;
+    echo "\nLa moyenne des notes supérieur ou égales à 10 est : $moy";
+}else{
+    echo "\nIl n'y a aucune moyenne supérieur à 10 !";
 }
---
+++ sequence4/tableau-index/temperature.php
@@ -0,0 +1,7 @@
+<?php
+$celcus=[25,35,41,12,5,9];
+foreach ($celcus as $c){
+    $resultat = ($c * 1.8) + 32;
+    echo "[$resultat °F]  ";
+}
+
